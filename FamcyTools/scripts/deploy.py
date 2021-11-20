947e12596cd533d53a7156677b1a1971ebe15f82
Weitung Chen <weitung@mit.edu>
2021-11-20 15:00:46 -0500

    update deploy websocket

--- FamcyTools/scripts/deploy.py
+++ FamcyTools/scripts/deploy.py
@@ -6,20 +6,26 @@ import FamcyTools
 LOCAL_USER = "/home/%U/.local/share/famcy"
 
 def main(args):
+	try:
+		socket_port = args[1]
+	except:
+		print("[ERROR] Please provide port number. e.g. famcy deploy pms 9090")
+		return
+
 	# Write famcy.ini
 	content = """[uwsgi]
 module = wsgi:app
 master = true
 processes = 1
-socket = /tmp/%s.sock
+http-socket = :%s
 chmod-socket = 660
 vacuum = true
 die-on-term = true
 enable-threads = true
 single-interpreter = true
-http-websockets = false
+http-websockets = true
 logto = %s
-log-maxsize = 2048000""" % (args[0], LOCAL_USER + "/" + args[0] + "/logs" + """/famcy.log""")
+log-maxsize = 2048000""" % (args[1], LOCAL_USER + "/" + args[0] + "/logs" + """/famcy.log""")
 
 	f = open(FamcyTools.FAMCY_DIR % (FamcyTools.USERNAME, args[0]) + "/famcy.ini", "w")
 	f.write(content)
@@ -59,12 +65,15 @@ WantedBy=multi-user.target
 	print("== Copy the following part to nginx configurations == (Need to change alias path if necessary)")
 	print("""
 location / {
-	include uwsgi_params;
-	uwsgi_pass unix:/tmp/%s.sock;
+    proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host $http_host;
+    proxy_redirect off;
+    uwsgi_max_temp_file_size 20480m;
+    proxy_pass http://127.0.0.1:%s;
 }
 
 location /static  {
     alias /home/%s/.local/share/famcy/%s/venv/lib/python3.7/site-packages/Famcy/static;
 }
-""" % (args[0], FamcyTools.USERNAME, args[0]))
+""" % (args[1], FamcyTools.USERNAME, args[0]))
 	print("Deployed to wsgi")
